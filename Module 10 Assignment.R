5dba310dcf59eec1e7db417a33218ba800c18308
jonathanrprogramming <35276790+jonathanrprogramming@users.noreply.github.com>
2018-03-21 19:10:51 -0400

    Add files via upload

---
+++ Module 10 Assignment.R
@@ -0,0 +1,8 @@
+Package: starwars
+Type: Package
+Title: Star Wars Love
+Version: 0.0.0.9000
+Authors@R: "Jonathan Whiteside <jwhiteside1@mail.usf.edu> [aut, cre]"
+Description: Displays simple passion for Star Wars
+Depends: R (>= 3.1.2)
+License: CCO
